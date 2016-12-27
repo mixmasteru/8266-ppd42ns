4844b3480cbb2534ff0b55bc4d0c1f775d33f6eb
Ulrich Pech <ulrichpech@gmail.com>
2016-12-27 14:17:36 +0100

    new readout code

--- src/examples/8266v1.ino
+++ src/examples/8266v1.ino
@@ -1,3 +1,5 @@
+#include "Arduino.h"
+
 /*
  Shinyei PPD42NS Particle Sensor
 
@@ -5,16 +7,16 @@
 
  specification: http://www.sca-shinyei.com/pdf/PPD42NS.pdf
 
- pin 1 (grey)  -> GND
- pin 3 (black) -> Vin
- pin 2 (green) -> Pin D5 / GPIO14
- pin 4 (white) -> Pin D6 / GPIO12
+ pin 1 (green)  -> GND
+ pin 3 (yellow) -> Vin
+ pin 2 (white) -> Pin D0
+ pin 4 (black) -> Pin D1
  pin 5 (red)   -> unused!
 
 */
 
-int pin1 = 5;
-int pin2 = 6;
+int pin1 = D0;
+int pin2 = D1;
 
 boolean valP1 = HIGH;
 boolean valP2 = HIGH;
@@ -28,12 +30,12 @@ boolean trigP2 = false;
 unsigned long trigOnP1;
 unsigned long trigOnP2;
 
-unsigned long sampletime_ms = 15000;
+unsigned long sampletime_ms = 20000;
 unsigned long lowpulseoccupancyP1 = 0;
 unsigned long lowpulseoccupancyP2 = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println("start PPD42NS");
   pinMode(pin1,INPUT);
   pinMode(pin2,INPUT);
@@ -51,18 +53,18 @@ void loop() {
     trigP1 = true;
     trigOnP1 = micros();
   }
-  
+
   if (valP1 == HIGH && trigP1 == true){
     durationP1 = micros() - trigOnP1;
     lowpulseoccupancyP1 = lowpulseoccupancyP1 + durationP1;
     trigP1 = false;
   }
-  
+
   if(valP2 == LOW && trigP2 == false){
     trigP2 = true;
     trigOnP2 = micros();
   }
-  
+
   if (valP2 == HIGH && trigP2 == true){
     durationP2 = micros() - trigOnP2;
     lowpulseoccupancyP2 = lowpulseoccupancyP2 + durationP2;
@@ -75,9 +77,9 @@ void loop() {
     concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // spec sheet curve
     Serial.print("lowpulseoccupancyP1:");
     Serial.print(lowpulseoccupancyP1);
-    Serial.print(" ratioP1:");
+    Serial.print(" ratio P1:");
     Serial.print(ratio);
-    Serial.print(" countP1:");
+    Serial.print(" concentration P1:");
     Serial.println(concentration);
 
     ratio = lowpulseoccupancyP2/(sampletime_ms*10.0);
@@ -85,9 +87,9 @@ void loop() {
 
     Serial.print("lowpulseoccupancyP2:");
     Serial.print(lowpulseoccupancyP2);
-    Serial.print(" ratioP2:");
+    Serial.print(" ratio P2:");
     Serial.print(ratio);
-    Serial.print(" countP2:");
+    Serial.print(" concentration P2:");
     Serial.println(concentration);
 
     lowpulseoccupancyP1 = 0;
@@ -95,5 +97,3 @@ void loop() {
     starttime = millis();
   }
 }
-
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,99 +1,106 @@
-#include "Arduino.h"
-
-/*
- Shinyei PPD42NS Particle Sensor
-
- using nodemcu-LoLin board
-
- specification: http://www.sca-shinyei.com/pdf/PPD42NS.pdf
-
- pin 1 (green)  -> GND
- pin 3 (yellow) -> Vin
- pin 2 (white) -> Pin D0
- pin 4 (black) -> Pin D1
- pin 5 (red)   -> unused!
-
+/**
+ * ESP8266 Arduino Dust Sensor for Shinyei PPD42NS
+ * connect the sensor as follows :
+ * pin 1 (green)  -> GND
+ * pin 3 (yellow) -> Vin
+ * pin 2 (white) -> Pin D0
+ * pin 4 (black) -> Pin D1
+ * pin 5 (red)   -> unused!
+ *
+ * Based on: https://github.com/empierre/arduino/blob/master/DustSensor_Shinyei_PPD42NS.ino
+ * Datasheet:  http://www.sca-shinyei.com/pdf/PPD42NS.pdf
 */
 
-int pin1 = D0;
-int pin2 = D1;
+#include "Arduino.h"
+#include <SPI.h>
 
-boolean valP1 = HIGH;
-boolean valP2 = HIGH;
+#define CHILD_ID_DUST_PM10            0
+#define CHILD_ID_DUST_PM25            1
+#define DUST_SENSOR_DIGITAL_PIN_PM10  D1
+#define DUST_SENSOR_DIGITAL_PIN_PM25  D2
 
+unsigned long SLEEP_TIME = 30*1000; // Sleep time between reads (in milliseconds)
+//VARIABLES
+int val = 0;           // variable to store the value coming from the sensor
+float valDUSTPM25 =0.0;
+float lastDUSTPM25 =0.0;
+float valDUSTPM10 =0.0;
+float lastDUSTPM10 =0.0;
+unsigned long duration;
 unsigned long starttime;
-unsigned long durationP1;
-unsigned long durationP2;
+unsigned long endtime;
+unsigned long sampletime_ms = 30000;
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+long concentrationPM25 = 0;
+long concentrationPM10 = 0;
+int temp=20; //external temperature, if you can replace this with a DHT11 or better
+long ppmv;
 
-boolean trigP1 = false;
-boolean trigP2 = false;
-unsigned long trigOnP1;
-unsigned long trigOnP2;
-
-unsigned long sampletime_ms = 20000;
-unsigned long lowpulseoccupancyP1 = 0;
-unsigned long lowpulseoccupancyP2 = 0;
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println("start PPD42NS");
-  pinMode(pin1,INPUT);
-  pinMode(pin2,INPUT);
+long getPM(int DUST_SENSOR_DIGITAL_PIN) {
   starttime = millis();
+
+  while (1) {
+
+	  duration = pulseIn(DUST_SENSOR_DIGITAL_PIN, LOW);
+	  lowpulseoccupancy += duration;
+	  endtime = millis();
+
+	  if ((endtime-starttime) > sampletime_ms)
+	  {
+  		ratio = (lowpulseoccupancy-endtime+starttime)/(sampletime_ms*10.0);  // Integer percentage 0=>100
+                  long concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
+  		//Serial.print("lowpulseoccupancy:");
+  		//Serial.print(lowpulseoccupancy);
+  		//Serial.print("\n");
+  		//Serial.print("ratio:");
+  		//Serial.print(ratio);
+  		//Serial.print("\n");
+  		//Serial.print("PPDNS42:");
+  		//Serial.println(concentration);
+  		//Serial.print("\n");
+
+  		lowpulseoccupancy = 0;
+  		return(concentration);
+	  }
+  }
 }
 
-void loop() {
-  float ratio = 0;
-  float concentration = 0;
+void setup()
+{
+  pinMode(DUST_SENSOR_DIGITAL_PIN_PM10,INPUT);
+  pinMode(DUST_SENSOR_DIGITAL_PIN_PM25,INPUT);
+  Serial.begin(115200);
+  Serial.println("start");
+}
 
-  valP1 = digitalRead(pin1);
-  valP2 = digitalRead(pin2);
+void loop()
+{
 
-  if(valP1 == LOW && trigP1 == false){
-    trigP1 = true;
-    trigOnP1 = micros();
+  //get PM 2.5 density of particles over 2.5 μm.
+  concentrationPM25=getPM(DUST_SENSOR_DIGITAL_PIN_PM25);
+  Serial.print("PM25: ");
+  Serial.println(concentrationPM25);
+  Serial.print("\n");
+  //ppmv=mg/m3 * (0.08205*Tmp)/Molecular_mass
+  //0.08205   = Universal gas constant in atm·m3/(kmol·K)
+  ppmv=(concentrationPM25*0.0283168/100/1000) *  (0.08205*temp)/0.01;
+
+  if ((ceil(concentrationPM25) != lastDUSTPM25)&&((long)concentrationPM25>0)) {
+      lastDUSTPM25 = ceil(concentrationPM25);
   }
 
-  if (valP1 == HIGH && trigP1 == true){
-    durationP1 = micros() - trigOnP1;
-    lowpulseoccupancyP1 = lowpulseoccupancyP1 + durationP1;
-    trigP1 = false;
-  }
+  //get PM 1.0 - density of particles over 1 μm.
+  concentrationPM10=getPM(DUST_SENSOR_DIGITAL_PIN_PM10);
+  Serial.print("PM10: ");
+  Serial.println(concentrationPM10);
+  Serial.print("\n");
 
-  if(valP2 == LOW && trigP2 == false){
-    trigP2 = true;
-    trigOnP2 = micros();
-  }
+  //ppmv=mg/m3 * (0.08205*Tmp)/Molecular_mass
+  //0.08205   = Universal gas constant in atm·m3/(kmol·K)
+  ppmv=(concentrationPM10*0.0283168/100/1000) *  (0.08205*temp)/0.01;
 
-  if (valP2 == HIGH && trigP2 == true){
-    durationP2 = micros() - trigOnP2;
-    lowpulseoccupancyP2 = lowpulseoccupancyP2 + durationP2;
-    trigP2 = false;
-  }
-
-  if ((millis()-starttime) > sampletime_ms)
-  {
-    ratio = lowpulseoccupancyP1/(sampletime_ms*10.0);                 // int percentage 0 to 100
-    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // spec sheet curve
-    Serial.print("lowpulseoccupancyP1:");
-    Serial.print(lowpulseoccupancyP1);
-    Serial.print(" ratio P1:");
-    Serial.print(ratio);
-    Serial.print(" concentration P1:");
-    Serial.println(concentration);
-
-    ratio = lowpulseoccupancyP2/(sampletime_ms*10.0);
-    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62;
-
-    Serial.print("lowpulseoccupancyP2:");
-    Serial.print(lowpulseoccupancyP2);
-    Serial.print(" ratio P2:");
-    Serial.print(ratio);
-    Serial.print(" concentration P2:");
-    Serial.println(concentration);
-
-    lowpulseoccupancyP1 = 0;
-    lowpulseoccupancyP2 = 0;
-    starttime = millis();
+  if ((ceil(concentrationPM10) != lastDUSTPM10)&&((long)concentrationPM10>0)) {
+      lastDUSTPM10 = ceil(concentrationPM10);
   }
 }
